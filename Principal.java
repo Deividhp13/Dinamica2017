1c00e09f005ea924636e72d7abc0a26073f8d54f
Deividhp13 <davidherrero96@gmail.com>
2017-12-13 13:52:54 +0100

    sdasd

--- Principal.java
+++ Principal.java
@@ -6,22 +6,23 @@ public  class Principal {
 
         //Array de enteros para almacenar las posiciones comunes.
         //Por cada acierto, sumo 1 al resultado anterior
-        int[][] elementosComunes = new int[m+1][n+1];
+        //int[][] elementosComunes = new int[m+1][n+1];
+
+        int contador1 = -1;
         int maxValorSubstring = Integer.MIN_VALUE, posFinalDelSubarray = -1;
         for(int i = 1; i < m+1; i++) {
             for(int j = 1; j < n+1; j++) {
                 if(s1.charAt(i-1) == s2.charAt(j-1)) {
-                    elementosComunes[i][j] = elementosComunes[i-1][j-1] + 1;
-                    if(elementosComunes[i][j] > maxValorSubstring) {
-                        maxValorSubstring = elementosComunes[i][j];
+                    contador1++;
+                    if(contador1 > maxValorSubstring) {
+                        maxValorSubstring = contador1;
                         posFinalDelSubarray = i;
                     }
                 }
                 else
-                    elementosComunes[i][j] = 0;
+                    contador1 = Integer.MIN_VALUE;
             }
         }
-
         return s1.substring(posFinalDelSubarray-maxValorSubstring,posFinalDelSubarray);
     }
 
