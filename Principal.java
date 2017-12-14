bb2adea51a8289e4a066ebef931ed70052de3495
Deividhp13 <davidherrero96@gmail.com>
2017-12-14 15:35:10 +0100

    Complejidad O(n)

--- Principal.java
+++ Principal.java
@@ -1,28 +1,45 @@
 public  class Principal {
 
-    public static String maxLong(String s1, String s2){
-        int m = s1.length();
-        int n = s2.length();
+    public static String maxLong(String s1, String s2) {
+        int mayor, menor;
 
-        //Array de enteros para almacenar las posiciones comunes.
-        //Por cada acierto, sumo 1 al resultado anterior
-        int[][] elementosComunes = new int[m+1][n+1];
-        int maxValorSubstring = Integer.MIN_VALUE, posFinalDelSubarray = -1;
-        for(int i = 1; i < m+1; i++) {
-            for(int j = 1; j < n+1; j++) {
-                if(s1.charAt(i-1) == s2.charAt(j-1)) {
-                    elementosComunes[i][j] = elementosComunes[i-1][j-1] + 1;
-                    if(elementosComunes[i][j] > maxValorSubstring) {
-                        maxValorSubstring = elementosComunes[i][j];
-                        posFinalDelSubarray = i;
-                    }
-                }
-                else
-                    elementosComunes[i][j] = 0;
-            }
+        if (s1.length() >= s2.length()) {
+            mayor = s1.length() + 1;
+            menor = s2.length() + 1;
+        } else {
+            mayor = s2.length() + 1;
+            menor = s1.length() + 1;
         }
 
-        return s1.substring(posFinalDelSubarray-maxValorSubstring,posFinalDelSubarray);
+        int cont = 0, pos = -1;
+        int[][] solucion = new int[2][mayor];
+
+        for (int i = 1; i < mayor; i++) {
+            for (int j = 1; j < menor; j++) {
+                if (s1.length() >= s2.length()) {
+                    if (s1.charAt(i - 1) == s2.charAt((j - 1))) {
+                        solucion[i % 2][j] = solucion[(i - 1) % 2][j - 1] + 1;
+                        if (solucion[i % 2][j] > cont) {
+                            cont = solucion[i % 2][j];
+                            pos = j;
+                        }
+                    } else
+                        solucion[i % 2][j] = 0;
+
+                } else {
+                    if (s2.charAt(i - 1) == s1.charAt((j - 1))) {
+                        solucion[i % 2][j] = solucion[(i - 1) % 2][j - 1] + 1;
+                        if (solucion[i % 2][j] > cont) {
+                            cont = solucion[i % 2][j];
+                            pos = j;
+                        }
+                    } else
+                        solucion[i % 2][j] = 0;
+                }
+            }
+        }
+        if (s1.length() >= s2.length()) return s2.substring((pos - cont), pos);
+        else return s1.substring((pos - cont), pos);
     }
 
 
